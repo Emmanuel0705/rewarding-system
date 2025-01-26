29093b37c668091ff0df07cd6c50a83f5226a8e0
Ajibola Oluwatobi <emmanuelloluwatobi@gmail.com>
2025-01-26 04:38:44 +0100

    continuing webhook integration

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,14 @@
 import express, { Request, Response } from "express";
-import { WebhookController } from "./controllers/webhook.controller";
-import { SignatureVerifier } from "./utils/signature-verifier";
+
+import bodyParser from "body-parser";
+import { handleWebhook } from "./webhook";
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 const port = 3020;
 
-const GITHUB_WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET || "";
-const webhookController = new WebhookController();
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.post(
-  "/webhooks/approved-pull-requests",
-  async (req: Request, res: Response): Promise<any> => {
-    try {
-      const payload = JSON.stringify(req.body);
-      console.log(payload);
-      const signature = req.get("X-Hub-Signature-256") || "";
-
-      // Verify webhook signature
-      const isValidSignature = SignatureVerifier.verifyWebhookSignature(
-        payload,
-        "5Fvqmk63bAQCTBeDmCQcQfuZJq1Lvywu3569MtAotSw2CRxr",
-        signature
-      );
-      if (!isValidSignature) {
-        return res.status(403).send("Invalid webhook signature");
-      }
-
-      console.log("Webhook signature verified--");
-
-      // Process the webhook
-      webhookController.handleMergedPullRequest(req.body);
-      res.status(200).send("Webhook processed successfully");
-    } catch (error) {
-      console.error("Webhook processing error", error);
-      res.status(500).send("Internal server error");
-    }
-  }
-);
+app.post("/webhook", handleWebhook);
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
